f55646571a17cbf3aa0c0a39670eb2fc3799229e
JangTaeGyu <ttggbbgg2@gmail.com>
2023-09-06 10:03:45 +0900

    dynamic components and import

--- app.vue
+++ app.vue
@@ -1,4 +1,17 @@
 <template>
-  <HomeWelcome />
-  <Counter />
+  <a @click="view('home')">Home</a>
+  <a @click="view('counter')">Counter</a>
+
+  <div>
+    <LazyHomeWelcome v-if="page === 'home'" />
+    <LazyCounter v-else-if="page === 'counter'" />
+  </div>
 </template>
+
+<script setup>
+const page = ref('home');
+
+function view(pageName) {
+  page.value = pageName;
+}
+</script>
