bed40affd61462fe758e432077b07457cfb2af1f
JangTaeGyu <ttggbbgg2@gmail.com>
2023-09-06 09:34:50 +0900

    auto import

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    Hello World
-  </div>
+  <div>Hello World</div>
+  <button @click="increment">Increment</button>
+  <p>{{ count }}</p>
+  <button @click="decrement">Decrement</button>
 </template>
+
+<script setup>
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
+function decrement() {
+  if (count.value > 0) count.value--;
+}
+
+</script>
