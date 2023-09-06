20f38111a25b2d926f1a97f661e8c485acd9848f
JangTaeGyu <ttggbbgg2@gmail.com>
2023-09-06 09:50:34 +0900

    components directory

--- app.vue
+++ app.vue
@@ -1,19 +1,4 @@
 <template>
-  <div>Hello World</div>
-  <button @click="increment">Increment</button>
-  <p>{{ count }}</p>
-  <button @click="decrement">Decrement</button>
+  <HomeWelcome />
+  <Counter />
 </template>
-
-<script setup>
-const count = ref(0);
-
-function increment() {
-  count.value++;
-}
-
-function decrement() {
-  if (count.value > 0) count.value--;
-}
-
-</script>
