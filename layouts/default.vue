6f89fdd3714a88afbd2912abb3a95c7a600d900a
JangTaeGyu <ttggbbgg2@gmail.com>
2023-09-06 15:33:57 +0900

    public directory

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <nav class="bg-purple-700 text-white">
+    <nav class="bg-purple-900 shadow-lg p-3 text-white text-lg flex justify-between">
       <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="/about">About</NuxtLink>
+      <NuxtLink to="/iphones">Iphones</NuxtLink>
       <NuxtLink to="/profile">Profile</NuxtLink>
     </nav>
 
---
+++ pages/iphones/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="grid grid-cols-4 gap-4 mt-10">
+    <NuxtLink
+        v-for="iphone in iphones"
+        :key="iphone"
+        :to="`/iphones/${iphone}`"
+        class="shadow-lg border rounded-lg text-center"
+    >
+      <h1>{{ iphone }}</h1>
+      <div class="flex justify-center">
+        <img width="200" :src="`/images/${iphone}.webp`" alt="" />
+      </div>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "Nuxt3 - Iphones",
+});
+
+const iphones = ["iphone-12", "iphone-12-pro", "iphone-13", "iphone-13-pro"];
+
+</script>`
